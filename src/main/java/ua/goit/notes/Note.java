2609e9c6ee8e17c695561b1eada02caebf7407bf
Inna Khomenko <itm.ikhomenko@gmail.com>
2022-02-10 11:21:05 +0200

    first commit note

---
+++ src/main/java/ua/goit/notes/Note.java
@@ -0,0 +1,38 @@
+/*
+package ua.goit.notes;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import ua.goit.base.BaseEntity;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "notes")
+public class Note implements BaseEntity<UUID> {
+
+    private static final long serialVersionUID = 3728386939882596015L;
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Type(type = "uuid-char")
+    @Column(name = "id", columnDefinition = "VARCHAR(36)")
+    private UUID id;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "description", length = 10000)
+    private String description;
+}
+*/
