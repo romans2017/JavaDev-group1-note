722d67de62d8b444a40e6d0e7abf7aebc819fd80
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2022-02-11 16:21:30 +0200

    add repository,controller,service for Note

---
+++ src/main/java/ua/goit/notes/NoteController.java
@@ -0,0 +1,21 @@
+package ua.goit.notes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class NoteController {
+    @Autowired
+    private NoteService noteService;
+
+    @GetMapping("/notes")
+    public String getAllNotes(Model model){
+        List<Note> all = noteService.findAll();
+        model.addAttribute("notes",all);
+        return "notes";
+    }
+}
--- src/main/resources/db/migration/h2/V2.0.0__Iint.sql
+++ src/main/resources/db/migration/h2/V2.0.0__Iint.sql
@@ -1,2 +1,4 @@
 
 CREATE TABLE notes (id UUID DEFAULT RANDOM_UUID() NOT NULL, description varchar(10000), name varchar(100), primary key (id));
+INSERT INTO notes(description,name) VALUES('some text','My note');
+
