7502ba21883aaa51c351cb0712cd3b9f8461621a
Tanya-PC <tanyuha942@gmail.com>
2022-02-18 01:44:35 +0200

    Refactor registration

--- src/main/java/ua/goit/users/RegistrationController.java
+++ src/main/java/ua/goit/users/RegistrationController.java
@@ -2,8 +2,8 @@ package ua.goit.users;
 
 import java.util.List;
 import javax.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -16,57 +16,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import ua.goit.roles.Role;
 import ua.goit.roles.RoleRepository;
 
+@RequiredArgsConstructor
 @Controller
-@RequestMapping("/registration")
+@RequestMapping("registration")
 public class RegistrationController {
 
-  private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private final UserRepository userRepository;
+    @Autowired
+    private final RoleRepository roleRepository;
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
 
-  @Autowired
-  private UserRepository userRepository;
-  @Autowired
-  private RoleRepository roleRepository;
-  @Autowired
-  private PasswordEncoder passwordEncoder;
-
-  private List<Role> initRoles() {
-    return roleRepository.findAll();
-  }
-
-  @GetMapping
-  public String registration(Model model) {
-    model.addAttribute("user", new User());
-    model.addAttribute("getRoles", initRoles());
-    return "registration";
-  }
-
-  @PostMapping
-  public String registrationUser(@Valid @ModelAttribute User user,
-                                  BindingResult result,
-                                  Model model) {
-
-    User userFromDb = userRepository.findByUserName(user.getUserName());
-    model.addAttribute("getRoles", initRoles());
-    if (result.hasErrors() || user.getRoles() == null || userFromDb != null) {
-      model.addAttribute("message", "Something wrong! Errors: " + result.getFieldErrors().size());
-      result
-          .getFieldErrors()
-          .forEach(f -> model.addAttribute(f.getField(), f.getDefaultMessage()));
-
-      result
-          .getFieldErrors()
-          .forEach(f -> System.out.println(f.getField() + ": " + f.getDefaultMessage()));
-      if (user.getRoles() == null) {
-        model.addAttribute("errorRoles", "User has minimum one role!");
-        return "registration";
-      } else if (userFromDb != null) {
-        model.addAttribute("errorUniqueUserName", "This user name is exists! User name must be unique!");
-        return "registration";
-      }
-      return "registration";
+    private List<Role> initRoles() {
+        return roleRepository.findAll();
     }
-    user.setPassword(passwordEncoder.encode(user.getPassword()));
-    userRepository.save(user);
-    return "redirect:/login";
-  }
+
+    private List<Role> getRoleByDefault() {
+        return initRoles().stream()
+                .filter(roleDefault -> roleDefault.getName().equals("user"))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping
+    public String registration(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostMapping
+    public String registrationUser(@Valid @ModelAttribute User user,
+                                   BindingResult result,
+                                   Model model) {
+
+        User userFromDb = userRepository.findByName(user.getName());
+        if (result.hasErrors() || userFromDb != null) {
+            model.addAttribute("message", "Something wrong! Errors: " + result.getFieldErrors().size());
+            result
+                    .getFieldErrors()
+                    .forEach(f -> model.addAttribute(f.getField(), f.getDefaultMessage()));
+            if (userFromDb != null) {
+                model.addAttribute("errorUniqueUserName", "This user name is exists! User name must be unique!");
+                return "registration";
+            }
+            return "registration";
+        }
+        user.setRoles(getRoleByDefault());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "redirect:/login";
+    }
 }
